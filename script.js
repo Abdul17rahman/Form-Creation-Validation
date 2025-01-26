42242299b0b703d11496c7d38f78bb4304797b8a
Abdul Rahman <abdulnsamba@gmail.com>
2025-01-26 15:59:51 +0300

    added dom content

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("registration-form");
   const feedbackDiv = document.getElementById("form-feedback");
 
@@ -37,4 +37,4 @@ window.onload = function () {
 
     form.reset();
   });
-};
+});
